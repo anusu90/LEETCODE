03416d2785f0d7fe6a6c11b596948c4e5298d30e
anunay sinha <anusu90@gmail.com>
2021-03-01 23:13:43 +0530

    104

---
+++ 104.js
@@ -0,0 +1,9 @@
+// 104. Maximum Depth of Binary Tree
+
+var maxDepth = function (root) {
+    if (!root) return 0
+    if (root.left == null && root.right == null) return 1;
+    let leftD = maxDepth(root.left) + 1;
+    let rightD = maxDepth(root.right) + 1;
+    return Math.max(leftD, rightD);
+};
