3081fa8454fe301c658e70a4bc3c3cec2345c86b
anusu90 <anusu90@gmail.com>
2021-04-10 18:11:45 +0530

    dept of binary tree

--- 104.js
+++ 104.js
@@ -2,7 +2,7 @@
 
 var maxDepth = function (root) {
     if (!root) return 0
-    if (root.left == null && root.right == null) return 1;
+    if (root.left == null && root.right == null) return 1; // This line is actually not needed to find the depth of tree
     let leftD = maxDepth(root.left) + 1;
     let rightD = maxDepth(root.right) + 1;
     return Math.max(leftD, rightD);
