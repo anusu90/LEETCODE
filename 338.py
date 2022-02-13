8fff8820d38e13382e8000ddbf799035dd50e158
anunay <anusu90@gmail.com>
2022-02-13 19:58:49 +0530

    new sol added

---
+++ 338.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        sol = []
+        for x in range(n+1):
+            t=0
+            for i in range(32):
+                t+=(x>>i)&1
+            sol.append(t)
+        return sol
