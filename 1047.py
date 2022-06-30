75dabaf7b2ddf67051d27510e7450b09b863f504
anunay <anusu90@gmail.com>
2022-06-30 23:26:51 +0530

    new sol added

---
+++ 1047.py
@@ -0,0 +1,14 @@
+from turtle import clear
+
+
+class Solution:
+    def removeDuplicates(self, A: str) -> str:
+        stack=[]
+        for x in A:
+            shouldInsert=True
+            while len(stack)>0 and stack[-1]==x:
+                stack.pop()
+                shouldInsert=False
+            if shouldInsert:
+                stack.append(x)
+        return "".join(stack)
