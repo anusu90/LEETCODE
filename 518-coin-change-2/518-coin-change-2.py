49cf392fa8e572516dbf0862c9a6946ff5ebd1c8
ANUNAY SINHA <55510402+anusu90@users.noreply.github.com>
2022-08-06 20:37:46 +0530

    Time: 216 ms (82.25%), Space: 38.3 MB (23.42%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,15 +1,16 @@
 class Solution:
     def change(self, target: int, nums: List[int]) -> int:
         n=len(nums)
-        dp=[[0 for _ in range(target+1)] for _ in range(n+1)]
+        dp=[[-1 for _ in range(target+1)] for _ in range(n+1)]
         
-        for i in range(n+1):
-            dp[i][0]=1
+        def recurr(n,k):
+            if k==0:
+                dp[n][k]=1
+            elif n==0:
+                dp[n][k]=0
+            if dp[n][k]==-1:
+                dp[n][k] = recurr(n-1,k) + (0 if nums[n-1]>k else recurr(n,k-nums[n-1]))
+            return dp[n][k]
         
-        
-        for j in range(1,target+1):
-            for i in range(1,n+1):
-                dp[i][j]=dp[i-1][j]+dp[i][j-nums[i-1]] if j>=nums[i-1] else dp[i-1][j]
-                # print(dp)
-        return dp[-1][-1]
+        return recurr(n,target)
         
