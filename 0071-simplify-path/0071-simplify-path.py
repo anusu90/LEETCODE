dfd86afe2bd81fc2a7ef8fffa3d06f2df3f2ae51
ANUNAY SINHA <55510402+anusu90@users.noreply.github.com>
2023-04-12 21:22:10 +0530

    Time: 38 ms (38.74%), Space: 13.8 MB (69.41%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,19 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack=[]
+        p = path.split("/")
+        hashMap={
+            "":1,
+            ".":1,
+            "..":1,
+            "/":1
+        }
+
+        for x in p:
+            if x not in hashMap:
+                stack.append(x)
+            elif x == ".." and len(stack)>0:
+                stack.pop()
+        if len(stack)==0:
+            return "/"
+        return "/" + "/".join(stack)
