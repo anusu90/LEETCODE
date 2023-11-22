3c717b5650e868b7bd59c137b57a5f322f820cbb
ANUNAY SINHA <55510402+anusu90@users.noreply.github.com>
2023-11-22 16:58:01 +0530

    Time: 318 ms (10.30%), Space: 89.3 MB (54.32%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -4,17 +4,23 @@
  * @return {Array}
  */
 var flat = function (arr, n) {
-    let sol = []
+    let sol =[]
     function recurr(arr,d){
+        console.log(arr.length,d)
+        if(d>n){
+            sol.push(arr);
+            return
+        }
         for(let i=0;i<arr.length;i++){
-            if(Array.isArray(arr[i]) && d<n){
+            if(Array.isArray(arr[i])){
                 recurr(arr[i],d+1)
             }else{
                 sol.push(arr[i])
             }
-        }
+        } 
     }
     recurr(arr,0);
     return sol;
-    
+    recurr(arr2,0)
+    return sol;
 };
